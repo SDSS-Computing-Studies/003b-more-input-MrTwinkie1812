3a527757a357757504c2bd47fdf47d47b1f81a47
MrTwinkie1812 <94935761+MrTwinkie1812@users.noreply.github.com>
2022-01-11 17:58:58 -0800

    need more help
    dont under stand how to use growth rate

--- task2.py
+++ task2.py
@@ -1,31 +1,47 @@
 #!python3
-"""
-Assignment: Exchange rate
-The current exchange rate is 1.25 CAD per 1 USD
-Create a program that asks the user for the number of Canadian Dollars they have
-and then have the program display how many USD that is equivalent to:
-You may need to use rounding or decimal formatting
+import math
+
+#A population can be modeled by the following:
+#future population = (current population)*(1+r)^(time in years) 
+#Have the user enter the current population, the rate of growth as a decimal and the number of DAYS.
+#Calculate the expected future population
 
 
-example
-How many Canadian Dollars do you have? 10
-That is worth $8.00 USD
+#Enter the population: 25000000
 
-How many Canadian Dollars do you have? 1.25
-That is worth $1.00 USD
-"""
+
+#Enter the rate of growth in percent: 2.1
+
+
+#Enter the number of days: 12
+
+
+#There will be 25017087 people after 12 days
+
+
+#current population
 input_1 = input("Enter the population:")
+
+#1+rate
 input_2 = input("Enter the rate of growth in percent:")
+
+#time in years 0.0328767
 input_3 = input("Enter the number of days:")
 
 
 a = int(input_1) 
-b = int(input_2)
-c = int(input_3)
+b = float(input_2)
+c = float(input_3)
 
 
-interest = a * b * c
+interest = a * b / 100 * c / 365
 #days in the month / 365
 interest = str(interest)
-print(interest)
-input_4 = input("There will be"+ interest +"people after"+ c +" days")
+
+
+in3 = round(interest,1)
+
+
+
+#future population = (current population)*(1+r)^(time in years) 
+print ("There will be "+ in3 +" people after 12 days")
